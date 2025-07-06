cf72cf6757c25742e3a01765f5141b55df144abc
H-goemon <u10beriko@gmail.com>
2025-07-06 13:04:52 +0000

    色々

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@ BufferedSerial pc{USBTX, USBRX, 115200};
 CAN can{PA_11, PA_12, (int)1e6};
 CANMessage msg;
 
+//ピンの初期設定ゾーン
 //PWM出力するピンを初期化する
 //命名規則：x_y (x:ポート,y:fwd→前転,rev→後転)
 PwmOut a_fwd(PA_1);
@@ -18,7 +19,9 @@ PwmOut d_fwd(PA_10);
 PwmOut d_rev(PA_9);
 
 //デジタル出力するピンを初期化する
+//can_read→CAN.read()の真偽値を反映するインジケータ
 DigitalOut can_read(PB_6);
+//sig_receive→msg.id=(自分のID)の真偽値を反映するインジケータ
 DigitalOut sig_receive(PB_7);
 
 //デジタル入力するピンを初期化する
@@ -29,8 +32,27 @@ DigitalIn idset_4(PB_1);
 //ユーザSWの入力ピン
 DigitalIn sw(PA_5);
 
-int main(){
-  while(1){
+//変数・配列の初期設定
+//メインマイコンから送られてきた送信前のデータを復元し、代入する配列
+int16_t in_data[4] = {0, 0, 0, 0};
+//PWM出力のduty比を代入する配列
+float out_data[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 
+int main(){
+  const int CANID = 1*!idset_1 + 2*!idset_2 + 4*!idset_4;
+  while(1){
+    if(can.read(msg) && msg.id == CANID){
+      for(int i = 0; i<4; i++){
+
+      }
+    }
+    a_fwd = out_data[0];
+    a_rev = out_data[1];
+    b_fwd = out_data[2];
+    b_rev = out_data[3];
+    c_fwd = out_data[4];
+    c_rev = out_data[5];
+    d_fwd = out_data[6];
+    d_rev = out_data[7];
   }
 }
